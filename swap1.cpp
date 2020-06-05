4387ab91d3c223f5b193f9869a8921f6ce041815
Oshin06 <66421468+Oshin06@users.noreply.github.com>
2020-06-05 04:48:26 -0700

    swap1.cpp

---
+++ swap1.cpp
@@ -0,0 +1,26 @@
+#include<iostream.h>
+#include<conio.h>
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+   clrscr();
+   const int n=15;
+   string str1[n], str2[n];
+   cout<<"Enter a name of anything : ";
+   cin>>str1[];
+   int x;
+   x=strlen(str1);
+   for(int i=0; i<x; i++)
+   {
+       for(int j=x; j>0; j--)
+       {  
+           if(i+j==x)
+           {
+              swap(str[i],str[j]);
+           }
+       }
+   }
+   cout<<"swp"<<str1[n];
+   getch();
+}
